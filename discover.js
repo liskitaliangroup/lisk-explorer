55b6c1c1e3295a090c4de1d8c294f314734fc7fd
andreafspeziale <andreaf.speziale@gmail.com>
2016-12-12 10:03:13 +0100

    added public-key username delegate crawler

--- discover.js
+++ discover.js
@@ -9,7 +9,6 @@ var nodesVisited = {};
 var nodeToVisit = [];
 var totalOpenNodes = 0;
 var totalClosedNodes = 0;
-var total = 0;
 
 var saveNodesReport = function () {
     return new Promise(function (resolve, reject) {
@@ -34,6 +33,11 @@ var loadNodesReport = function () {
 var nodesReport = loadNodesReport();
 console.log(colors.magenta(new Date(Date.now()).toString()) + ' | ' + colors.green('Crawl started\n'));
 
+
+/*
+ * Check if the node in config.json has open API to use as network entry point
+*/
+
 var isStartingWithOpenApiNode = function() {
     return new Promise(function (resolve, reject) {
         request.get('http://' + startingNode + '/api/peers?state=2&orderBy=version:desc',{timeout: 3500}, function(error, response, body) {
@@ -54,7 +58,9 @@ isStartingWithOpenApiNode().then(function(res) {
     console.log(colors.magenta(new Date(Date.now()).toString()) + ' | ' + colors.red('Please start the explorer with an open API Lisk node'));
 });
 
-
+/*
+ * Crawling the network
+*/
 
 function crawl() {
     var nextNode = nodeToVisit.pop();
@@ -64,14 +70,25 @@ function crawl() {
         else
             visitNode(nextNode, crawl);
     } else {
+        /*
+         * Crawling finished start collecting publicKeys
+        */
         nodesReport.finish = new Date(Date.now()).toString();
         nodesReport.totalOpenNodes = totalOpenNodes;
         nodesReport.totalClosedNodes = totalClosedNodes;
         nodesReport.total = totalOpenNodes + totalClosedNodes;
-        saveNodesReport().then(function (res) {
-            console.log('\n' + res);
+        collectDelegatesPublicKey().then(function (res) {
+            /*
+             * publicKeys collected
+            */
+            console.log('\n'+colors.magenta(new Date(Date.now()).toString()) + ' | ' +colors.green(res));
+            saveNodesReport().then(function (res) {
+                console.log('\n' + res);
+            }, function (err) {
+                console.log(err);
+            })
         }, function (err) {
-            console.log(err);
+            reject(err);
         })
     }
 }
@@ -96,3 +113,52 @@ function visitNode(node, callback) {
         }
     })
 };
+
+/*
+ * Collecting all the delegates public key in the nodes.json
+*/
+
+var browseDelegate = function (pageCounter) {
+    return new Promise(function (resolve, reject) {
+        request('http://' + startingNode + '/api/delegates/?limit=101&offset=' + pageCounter + '&orderBy=rate:asc', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var res = JSON.parse(body)
+                if(res.delegates.length)
+                   resolve(res);
+            } else {
+                reject(error);
+            }
+        })
+    });
+};
+
+function collectDelegatesPublicKey() {
+    return new Promise(function (resolve, reject) {
+        var pageCounter = 0;
+        var numberOfDelegates = 0;
+        browseDelegate(pageCounter).then(function(res) {
+               numberOfDelegates = res.totalCount;
+               nodesReport.totalRegisteredDelegates = numberOfDelegates;
+               for(pageCounter; pageCounter < numberOfDelegates; pageCounter += 101) {
+                   browseDelegate(pageCounter).then(function(res) {
+                       var delegates = res;
+                       for (var i = 0; i < delegates.delegates.length; i++) {
+                           //console.log(delegates.delegates[i].username)
+                           nodesReport.delegatesPublicKey.push({
+                               "publicKey":delegates.delegates[i].publicKey,
+                               "username":delegates.delegates[i].username
+                           });
+                           resolve('Delegates crawled');
+                       }
+                   }, function (err) {
+                       console.log(err);
+                       reject(false);
+                   });
+               }
+           }, function (err) {
+               console.log(err);
+               reject(false);
+           });
+       }
+   );
+}
